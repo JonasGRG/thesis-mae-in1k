83d590ff99866c854a486e8487f95bf1287a61df
JonasGRG <jonas@gjerris.dk>
2024-11-19 14:58:46 +0100

    change to 2 gpus

--- batch_jobs/800/pretrain_in1k_0.75_800_8.sh
+++ batch_jobs/800/pretrain_in1k_0.75_800_8.sh
@@ -5,13 +5,13 @@
 ### -- set the job Name -- 
 #BSUB -J 800_run8
 ### -- ask for number of cores (default: 1) -- 
-#BSUB -n 12
+#BSUB -n 8
 ### -- specify that the cores must be on the same host -- 
 #BSUB -R "span[hosts=1]"
 ### -- specify that we need 3GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=3GB]"
-### -- Select the resources: 3 gpu in exclusive process mode --
-#BSUB -gpu "num=3:mode=exclusive_process"
+#BSUB -R "rusage[mem=4GB]"
+### -- Select the resources: 2 gpu in exclusive process mode --
+#BSUB -gpu "num=2:mode=exclusive_process"
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 24:00 
 ### -- Specify the output and error file. %J is the job-id -- 
